571ab437e74c59de128cffcd5a282f2097c4b567
Kristian <kristian.hamilton@signavio.com>
2020-09-15 11:00:24 +0200

    Updates

---
+++ precache-manifest.447c3212fee6ee7a6eb7a136d580b9b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249f58d5d8a86a83714891b33a898048",
+    "url": "/twentyone/index.html"
+  },
+  {
+    "revision": "4081e7948c652c681ee5",
+    "url": "/twentyone/static/css/main.a7603c93.chunk.css"
+  },
+  {
+    "revision": "534c0e70f42b17ef7fa9",
+    "url": "/twentyone/static/js/2.91eeffb7.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/twentyone/static/js/2.91eeffb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4081e7948c652c681ee5",
+    "url": "/twentyone/static/js/main.979bf77a.chunk.js"
+  },
+  {
+    "revision": "9de0bff5d3d6bf3fa3cf",
+    "url": "/twentyone/static/js/runtime-main.5759e428.js"
+  },
+  {
+    "revision": "235b7008a3d9bdcea2a99dfe952dca51",
+    "url": "/twentyone/static/media/face-down-card.235b7008.png"
+  },
+  {
+    "revision": "ba6ed8b6fe5ae62cc15de8b588e78fcb",
+    "url": "/twentyone/static/media/playingCards.ba6ed8b6.png"
+  }
+]);
